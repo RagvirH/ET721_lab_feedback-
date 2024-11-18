927654eb99407f40c455c9e6255376d97d25a5a2
RagvirH <157735804+RagvirH@users.noreply.github.com>
2024-11-18 18:45:54 +0000

    part 3

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Feedback(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback for {self.item.name}"
