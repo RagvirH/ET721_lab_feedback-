6bf7b20f5f1959bf8a84c7d3511d4dc4271516ba
RagvirH <157735804+RagvirH@users.noreply.github.com>
2024-12-13 18:11:02 +0000

    something

--- feedback/models.py
+++ feedback/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-
-
-class Feedback(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    comment = models.TextField()
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+class FeedbackItem(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Feedback for {self.item.name}"
+        return self.title
--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import FeedbackItem
 
-# Create your views here.
+def home(request):
+    return render(request, 'feedback/home.html')
+
+def item_list(request):
+    items = FeedbackItem.objects.all()
+    return render(request, 'feedback/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(FeedbackItem, pk=pk)
+    return render(request, 'feedback/item_detail.html', {'item': item})
